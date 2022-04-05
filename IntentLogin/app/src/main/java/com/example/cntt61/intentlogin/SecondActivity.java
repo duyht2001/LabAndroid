cfb8f499a11f4d14a24e61f11ce6c3857a09ce1e
duyht2001 <hoangthaiduy@hotmail.com>
2022-04-05 09:05:42 +0700

    update lai bai Intent

---
+++ IntentLogin/app/src/main/java/com/example/cntt61/intentlogin/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.example.cntt61.intentlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button btnSecond;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        btnSecond = (Button) findViewById(R.id.buttonSecond);
+
+        btnSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
